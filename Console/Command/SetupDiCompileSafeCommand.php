6d693353099d8749e1c55ae92e412e5461509e9e
Anael Ollier <aollier@clever-age.com>
2023-01-24 14:57:26 +0100

    Bump copyright date

--- Console/Command/SetupDiCompileSafeCommand.php
+++ Console/Command/SetupDiCompileSafeCommand.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the CleverAge/Tools package.
  *
- * Copyright (C) 2020-2021 Clever-Age
+ * Copyright (C) 2020-2023 Clever-Age
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
